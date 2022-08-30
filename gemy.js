a28b145f60dbc6d99e759c6d896633e8d61c1810
Rayan <mahamedrayan2@gmail.com>
2022-08-30 20:23:42 +0200

    file

---
+++ gemy.js
@@ -0,0 +1,9 @@
+let img = document.querySelector(".img");
+let baba = document.querySelector(".header");
+
+function rayan(rayano) {
+  img.src = rayano;
+}
+function header(khalfea, opacity) {
+  baba.style.background = khalfea;
+}
